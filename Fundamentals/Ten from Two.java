020301ecefffbeadf26de2f19748db295f3d5a3a
Parth Parmar <parthparmar661@gmail.com>
2023-06-03 16:37:53 +0530

    Create Ten from Two.java

---
+++ Fundamentals/Ten from Two.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner sc= new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-->0)
+      {
+        int n=sc.nextInt();
+        if(n%10==0)
+        System.out.println("0");
+        else if(n%10==5)
+        System.out.println("1");
+        else System.out.println("-1");
+      }
+    }
+  }
